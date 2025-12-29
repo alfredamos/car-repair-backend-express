973b9bbb5158ec94c288e0e96700d7351f869e96
Alfred Akintunde AMOS <52054578+alfredamos@users.noreply.github.com>
2025-12-29 23:55:29 +0100

    Add ticket model with CRUD functionalities.

---
+++ src/models/ticket.model.ts
@@ -0,0 +1,116 @@
+import { prisma } from "../db/db";
+import catchError from "http-errors";
+import { StatusCode } from "http-status-codes";
+import { Ticket } from "../generated/prisma/client";
+import { ResponseMessage } from "../utils/responseMessage.util";
+import { TicketQueryCondition } from "../utils/ticketQueryCondition";
+
+class TicketModel {
+    async changeTicketStatus(id: string){
+        //----> Fetch ticket by given id.
+        const ticket = await this.getOneTicket(id);
+
+        //----> Change ticket status.
+        ticket.completed = !ticket.completed;
+
+        //----> Update ticket in db.
+        await prisma.ticket.update({
+            where: { id },
+            data: ticket
+        });
+
+        //----> Return updated ticket.
+        return ticket;
+    }
+
+    async createTicket(ticket: Ticket){
+        //----> Store the new ticket in the database.
+        return await prisma.ticket.create({ data: ticket });
+    }
+
+    async deleteTicketById(id: string){
+        //----> Fetch ticket by given id.
+        await this.getOneTicket(id);
+
+        //----> Delete ticket from db.
+        await prisma.ticket.delete({ where: { id }});
+
+        //----> Return feedback.
+        return new ResponseMessage('Ticket deleted successfully!', 'success', StatusCode.OK);
+    }
+
+    async editTicketById(id: string, ticket: Ticket){
+        //----> Fetch ticket by given id.
+        await this.getOneTicket(id);
+
+        //----> Edit ticket from db.
+        await prisma.ticket.update({ where: { id }, data: ticket});
+
+        //----> Return feedback.
+        return new ResponseMessage('Ticket edited successfully!', 'success', StatusCode.OK);
+    }
+
+    async getAllTickets(){
+        //----> Fetch all tickets from db.
+        return await prisma.ticket.findMany();
+    }
+
+    async getCompletedTickets(){
+        //----> Fetch all completed tickets from db.
+        const query: TicketQueryCondition = { completed: true };
+        return await this.getTicketByQueryCondition(query);
+    }
+
+    async getInCompleteTickets(){
+        //----> Fetch all incompleted tickets from db.
+        const query: TicketQueryCondition = { completed: false };
+        return await this.getTicketByQueryCondition(query);
+    }
+
+    async getTicketsByCustomerId(customerId: string){
+        //----> Fetch tickets by customer id.
+        const query: TicketQueryCondition = { customerId };
+        return await this.getTicketByQueryCondition(query);
+    }
+
+    async getTicketsByUserEmail(email){
+        const query: TicketQueryCondition = { tech: email };
+        return await this.getTicketByQueryCondition(query);
+    }
+
+    async getTicketById(id: string){
+        //----> Fetch ticket by given id.
+        return await this.getOneTicket(id);
+    }
+
+    private async getOneTicket(id:string){
+        //----> Fetch ticket by id
+        const ticket = await prisma.ticket.findUnique({
+            where: { id }
+        });
+
+        //----> Check for null ticket.
+        if (!ticket) {
+            throw catchError(StatusCode.NOT_FOUND, 'Ticket not found');
+        }
+
+        //----> Return ticket.
+        return ticket;
+    }
+
+    private async getTicketByQueryCondition(query: TicketQueryCondition){
+        //----> Fetch tickets by query condition.
+        const tickets = await prisma.ticket.findMany({where: { ...query }})
+
+        //----> Check for empty tickets.
+        if (!tickets?.length) {
+            throw catchError(StatusCode.NOT_FOUND, 'No tickets match query condition!');
+        }
+
+        //----> Return tickets.
+        return tickets;
+    }
+
+
+
+}
